9168d2e9067512c66e6abfe9d37b9739851ce397
Seb Bacon <seb.bacon@gmail.com>
2016-11-21 10:45:47 +0000

    Keep track with ever-changing NHS digital URLs

--- fetchers/hscic_prescribing.py
+++ fetchers/hscic_prescribing.py
@@ -123,7 +123,7 @@ class Command(BaseCommand):
         ]
 
     def most_recent_date(self):
-        url = ('http://digital.nhs.uk'
+        url = ('http://content.digital.nhs.uk'
                '/searchcatalogue?'
                'q=title%3a%22presentation+level+data%22&sort=Most+recent')
         page = requests.get(url)
